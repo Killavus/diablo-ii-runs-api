adbbbb02e2bf4246f848513992082acd180863c6
Killavus <killavus@gmail.com>
2021-11-18 10:55:24 +0100

    make CORS configurable using environment variables

--- src/main.rs
+++ src/main.rs
@@ -26,12 +26,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
     let handler_ctx = wrap_fn(context::HandlerCtx::request_id_wrapper);
     let db_pool = database::pool().await?;
+    let origin_list =
+        std::env::var("RUNS_API_CORS_ORIGINS").unwrap_or("http://localhost:3000".into());
 
-    let cors = warp::cors()
+    let mut cors = warp::cors()
         .allow_headers(vec![header::CONTENT_TYPE])
-        .allow_methods(vec![Method::POST, Method::GET])
-        .allow_origin("http://localhost:3000")
-        .build();
+        .allow_methods(vec![Method::POST, Method::GET]);
+
+    for origin in origin_list.split(",") {
+        let origin = origin.trim();
+        cors = cors.allow_origin(origin);
+    }
 
     let routes = api::root(db_pool)
         .with(handler_ctx)
